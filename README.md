49c449bd473600127b20f566b296639f7a8b3de0
Mateus Lopes <73197343+progmateus@users.noreply.github.com>
2023-03-01 16:06:31 -0300

    fixing docker

--- README.md
+++ README.md
@@ -19,28 +19,7 @@ yarn typeorm migration:run
 
 ```
 
-## Se houver problemas com o Docker execute da seguinte forma
 
-```bash
-# clonar repositório
-git clone https://github.com/progmateus/api-hiper-challenge.git
-
-# entrar na pasta do projeto front end web
-cd api-hiper-challenge
-
-# instalar dependências
-yarn install
-
-# rodar o banco de dados postgres
-docker compose up database
-
-# rodar as migrations do banco de dados
-yarn typeorm migration:run
-
-# executar o projeto
-yarn dev
-
-```
 # Autor
 
 Mateus Lopes
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm"
 
-export default async (): Promise<Connection> => {
+export default async (host = "hipersend_database"): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions()
     return createConnection(
         Object.assign(defaultOptions, {
-            database: process.env.NODE_ENV === "test"
-                ? "super_price_test"
-                : defaultOptions.database
+            host
         })
     )
 }
